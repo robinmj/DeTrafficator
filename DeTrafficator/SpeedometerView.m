e7ed8e9138eca16f98fdf334d2906781364f805c
Robin Johnson <robin@e7systems.com>
2013-09-09 11:30:17 -0600

    -removed gradient, renamed circle, added speed markings

--- DeTrafficator/SpeedometerView.m
+++ DeTrafficator/SpeedometerView.m
@@ -50,47 +50,33 @@
     
     debug_NSLog(@"initWithParentView()");
     
-    // the gradient
-    CAGradientLayer* g = [[CAGradientLayer alloc] init];
-    g.frame = self.bounds;
-    g.colors = [NSArray arrayWithObjects:
-                (id)[[UIColor blackColor] CGColor],
-                [[UIColor redColor] CGColor],
-                nil];
-    g.locations = [NSArray arrayWithObjects:
-                   [NSNumber numberWithFloat: 0.0], [NSNumber numberWithFloat: 1.0], nil];
-    [self addSublayer:g];
-    // the circle
-    CAShapeLayer* circle = [[CAShapeLayer alloc] init];
-    circle.lineWidth = 2.0;
-    circle.fillColor =
-    [[UIColor colorWithRed:0.9 green:0.95 blue:0.93 alpha:0.9] CGColor]; circle.strokeColor = [[UIColor grayColor] CGColor];
-    CGMutablePathRef p = CGPathCreateMutable(); CGPathAddEllipseInRect(p, NULL, CGRectInset(self.bounds, 3, 3));
-    circle.path = p;
-    [self addSublayer:circle];
-    circle.bounds = self.bounds;
-    circle.position = CGPointMake(CGRectGetMidX(self.bounds),
+    // the speedometer gauge
+    CAShapeLayer* arc = [[CAShapeLayer alloc] init];
+    arc.lineWidth = 2.0;
+    arc.fillColor =
+    [[UIColor colorWithRed:0.9 green:0.95 blue:0.93 alpha:0.9] CGColor];
+    arc.strokeColor = [[UIColor grayColor] CGColor];
+    CGMutablePathRef p = CGPathCreateMutable();
+    CGPathAddEllipseInRect(p, NULL, CGRectInset(self.bounds, 3, 3));
+    arc.path = p;
+    [self addSublayer:arc];
+    arc.bounds = self.bounds;
+    arc.position = CGPointMake(CGRectGetMidX(self.bounds),
                                   CGRectGetMidY(self.bounds));
-    // the four cardinal points
-    NSArray* pts = [NSArray arrayWithObjects: @"N", @"E", @"S", @"W", nil]; for (int i = 0; i < 4; i++) {
-        CATextLayer* t = [[CATextLayer alloc] init]; t.string = [pts objectAtIndex: i];
-        t.bounds = CGRectMake(0,0,40,30);
-        t.position = CGPointMake(CGRectGetMidX(circle.bounds),
-                                 CGRectGetMidY(circle.bounds));
-        CGFloat vert = (CGRectGetMidY(circle.bounds) - 5) / CGRectGetHeight(t.bounds);
+    
+    for (int i = 0; i < 9; i++) {
+        CATextLayer* t = [[CATextLayer alloc] init];
+        t.string = [NSString stringWithFormat:@"%i", i * 5];
+        t.bounds = CGRectMake(0,0,40,60);
+        t.position = CGPointMake(CGRectGetMidX(arc.bounds),
+                                 CGRectGetMidY(arc.bounds));
+        CGFloat vert = (CGRectGetMidY(arc.bounds) - 5) / CGRectGetHeight(t.bounds);
         t.anchorPoint = CGPointMake(0.5, vert);
         t.alignmentMode = kCAAlignmentCenter;
         t.foregroundColor = [[UIColor blackColor] CGColor];
-        [t setAffineTransform:CGAffineTransformMakeRotation(i*M_PI/2.0)];
-        [circle addSublayer:t]; }
-    // the arrow
-    CALayer* arrow = [[CALayer alloc] init];
-    arrow.bounds = CGRectMake(0, 0, 40, 100);
-    arrow.position = CGPointMake(CGRectGetMidX(self.bounds),
-                                 CGRectGetMidY(self.bounds)); arrow.anchorPoint = CGPointMake(0.5, 0.8);
-    arrow.delegate = self;
-    [arrow setAffineTransform:CGAffineTransformMakeRotation(M_PI/5.0)]; [self addSublayer:arrow];
-    [arrow setNeedsDisplay];
+        [t setAffineTransform:CGAffineTransformMakeRotation((i - 4)*M_PI/8.0)];
+        [arc addSublayer:t];
+    }
     
     return self;
 }
