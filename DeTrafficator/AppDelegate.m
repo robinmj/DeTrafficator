699f375c1cf1199e070c29d52354bfd3722d462b
Robin Johnson <robin.m.j@gmail.com>
2013-11-16 20:00:29 -0700

    -prevented screen from falling asleep when application is running

--- DeTrafficator/AppDelegate.m
+++ DeTrafficator/AppDelegate.m
@@ -15,6 +15,8 @@
 {
     [SpeedometerView class];
     
+    application.idleTimerDisabled = YES;
+    
     // Override point for customization after application launch.
     return YES;
 }
@@ -29,11 +31,15 @@
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    application.idleTimerDisabled = NO;
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    
+    application.idleTimerDisabled = YES;
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
